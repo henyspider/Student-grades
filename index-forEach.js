11f8f7e6b6593733dcab41c9616be7fc0238d685
Software Softwarewebdeveloper Web developer <softwarewebdeveloper@MacBookHenri.local>
2021-06-25 20:31:24 +0200

    Adding a new copy of index-forOf, called index-forEach

---
+++ index-forEach.js
@@ -0,0 +1,58 @@
+const students = [
+	{
+		name: "Peyton",
+		grade: 47,
+	},
+	{
+		name: "Emily",
+		grade: 56,
+	},
+	{
+		name: "Claus",
+		grade: 35,
+	},
+	{
+		name: "Nelly",
+		grade: 72,
+	},
+	{
+		name: "Brandon",
+		grade: 80,
+	},
+	{
+		name: "Trent",
+		grade: 79,
+	},
+	{
+		name: "Chris",
+		grade: 61,
+	},
+	{
+		name: "James",
+		grade: 60,
+	},
+	{
+		name: "Alex",
+		grade: 86,
+	},
+	{
+		name: "Khloe",
+		grade: 97,
+	},
+];
+
+const passingScore = 60;
+
+function getPassingStudents(students) {
+	let passingStudents = [];
+
+	for (const student of students) {
+		if (student.grade >= passingScore) {
+			passingStudents.push(student.name);
+		}
+	}
+
+	return passingStudents;
+}
+
+console.log(getPassingStudents(students));
