c4420bfe90cac042638081033bf968e8bbab47b3
henyspider <79423784+henyspider@users.noreply.github.com>
2021-06-25 20:02:09 +0200

    Added formatting and logical changes
    I modified the name of the variables inside the for loops to have a more solid and more logical understanding of the code. I added some formatting as well.

--- index-forOf.js
+++ index-forOf.js
@@ -1,60 +1,58 @@
 const students = [
-    {
-        name : "Peyton",
-        grade : 47
-    },
-    {
-        name : "Emily",
-        grade : 56
-    },
-    {
-        name : "Claus",
-        grade : 35
-    },
-    {
-        name : "Nelly",
-        grade : 72
-    },
-    {
-        name : "Brandon",
-        grade : 80
-    },
-    {
-        name : "Trent",
-        grade : 79
-    },
-    {
-        name : "Chris",
-        grade : 61
-    },
-    {
-        name : "James",
-        grade : 60
-    },
-    {
-        name : "Alex",
-        grade : 86
-    },
-    {
-        name : "Khloe",
-        grade : 97
-    }
-]
+	{
+		name: "Peyton",
+		grade: 47,
+	},
+	{
+		name: "Emily",
+		grade: 56,
+	},
+	{
+		name: "Claus",
+		grade: 35,
+	},
+	{
+		name: "Nelly",
+		grade: 72,
+	},
+	{
+		name: "Brandon",
+		grade: 80,
+	},
+	{
+		name: "Trent",
+		grade: 79,
+	},
+	{
+		name: "Chris",
+		grade: 61,
+	},
+	{
+		name: "James",
+		grade: 60,
+	},
+	{
+		name: "Alex",
+		grade: 86,
+	},
+	{
+		name: "Khloe",
+		grade: 97,
+	},
+];
 
 const passingScore = 60;
 
-function getPassingStudents (arr){
+function getPassingStudents(students) {
+	let passingStudents = [];
 
-    let passingStudents = [];
+	for (const student of students) {
+		if (student.grade >= passingScore) {
+			passingStudents.push(student.name);
+		}
+	}
 
-    for (const obj of arr){
-
-        if (obj.grade >= passingScore){
-            passingStudents.push(obj.name)
-        }
-    }
-
-    return passingStudents
+	return passingStudents;
 }
 
 console.log(getPassingStudents(students));
