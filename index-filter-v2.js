26ee63b1faa541fb74ea90c952d94bd04c292f03
Software Softwarewebdeveloper Web developer <softwarewebdeveloper@MacBookHenri.local>
2021-06-26 14:02:02 +0200

    I'm uploading a modified version of the index-filter original file. In this version, the callback functions are all grouped in one function named getNamesOfPassingStudents, that way we only have one function to run the code instead of having several ones.

---
+++ index-filter-v2.js
@@ -0,0 +1,58 @@
+const students = [
+	{
+		name: "Peyton",
+		grade: 47,
+	},
+	{
+		name: "Emily",
+		grade: 56,
+	},
+	{
+		name: "Claus",
+		grade: 35,
+	},
+	{
+		name: "Nelly",
+		grade: 72,
+	},
+	{
+		name: "Brandon",
+		grade: 80,
+	},
+	{
+		name: "Trent",
+		grade: 79,
+	},
+	{
+		name: "Chris",
+		grade: 61,
+	},
+	{
+		name: "James",
+		grade: 60,
+	},
+	{
+		name: "Alex",
+		grade: 86,
+	},
+	{
+		name: "Khloe",
+		grade: 97,
+	},
+];
+
+const passingScore = 60;
+
+function getNamesOfPassingStudents(students) {
+	const passingStudents = students.filter((student) => {
+		return student.grade >= passingScore;
+	});
+
+	const namesOfPassingStudents = passingStudents.map((student) => {
+		return student.name;
+	});
+
+	return namesOfPassingStudents;
+}
+
+console.log(getNamesOfPassingStudents(students));
